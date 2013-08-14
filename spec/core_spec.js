94a2ac5094cdaf00857fdb0231285d29e58a8448
Nick Zhu <nick.qi.zhu@gmail.com>
2013-08-14 15:30:59 -0400

    add simple zoomable component blueprint

--- spec/core_spec.js
+++ spec/core_spec.js
@@ -9,20 +9,33 @@ describe('canvas', function () {
     var canvas;
 
     beforeEach(function () {
-        canvas = zui.canvas();
-        canvas.render();
+        canvas = d3.select(zui.canvas().render());
     });
 
     it('should generate svg canvas', function () {
-        expect(svg()).not.toBeEmpty();
+        expect(canvas).not.toBeEmpty();
     });
 
     it('should max svg width and height', function () {
-        expect(svg().attr("width")).toBe("100%");
-        expect(svg().attr("height")).toBe("100%");
+        expect(canvas.attr("width")).toBe("100%");
+        expect(canvas.attr("height")).toBe("100%");
+    });
+});
+
+describe('zoomable', function () {
+    var canvas;
+
+    beforeEach(function () {
+        canvas = d3.select(zui.canvas().render());
     });
 
-    function svg() {
-        return d3.select("body svg");
-    }
+    it('should generate svg:g in parent canvas', function () {
+        zui.zoomable(canvas).render();
+        expect(canvas.select("g")).not.toBeEmpty();
+    });
+
+    it('should generate svg:g in parent element', function () {
+        zui.zoomable(canvas.node()).render();
+        expect(canvas.select("g")).not.toBeEmpty();
+    });
 });
--- src/core.js
+++ src/core.js
@@ -2,15 +2,29 @@ var zui = {
     version: '<%=pkg.version%>'
 };
 
-zui.canvas = function(){
-    var that = {};
+zui.canvas = function () {
+    var _that = {};
 
-    that.render = function(){
-        d3.select('body')
+    _that.render = function () {
+        var svg = d3.select(document.createElement('div'))
             .append('svg')
             .attr('width', '100%')
             .attr('height', '100%');
+
+        return svg.node();
     };
 
-    return that;
+    return _that;
+};
+
+zui.zoomable = function (parent) {
+    var _that = {}, _parent = (parent instanceof d3.selection)?parent:d3.select(parent);
+
+    _that.render = function () {
+        var g = _parent.append('g');
+
+        return  g;
+    };
+
+    return _that;
 };
