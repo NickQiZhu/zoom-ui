ac0700dd3977daa3c1bb97aa58adc6e0d6fe1182
Nick Zhu <nick.qi.zhu@gmail.com>
2013-08-14 15:11:11 -0400

    add sizing to canvas

--- spec/core_spec.js
+++ spec/core_spec.js
@@ -1,13 +1,28 @@
-describe('core', function(){
-    it('has version', function(){
+describe('core', function () {
+    it('has version', function () {
         expect(zui.version).not.toBe(null);
     });
 });
 
-describe('canvas', function(){
-    it('should generate svg canvas', function(){
-        var canvas = zui.canvas();
+
+describe('canvas', function () {
+    var canvas;
+
+    beforeEach(function () {
+        canvas = zui.canvas();
         canvas.render();
-        expect(d3.select("body svg")).not.toBeEmpty();
     });
+
+    it('should generate svg canvas', function () {
+        expect(svg()).not.toBeEmpty();
+    });
+
+    it('should max svg width and height', function () {
+        expect(svg().attr("width")).toBe("100%");
+        expect(svg().attr("height")).toBe("100%");
+    });
+
+    function svg() {
+        return d3.select("body svg");
+    }
 });
--- zoom-ui.js
+++ zoom-ui.js
@@ -7,7 +7,9 @@ zui.canvas = function(){
 
     that.render = function(){
         d3.select('body')
-            .append('svg');
+            .append('svg')
+            .attr('width', '100%')
+            .attr('height', '100%');
     };
 
     return that;
